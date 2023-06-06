13e7926a2221c4d4dc3f631fdac86ecf603e20ad
Feza <fezaarnand@gmail.com>
2023-06-06 13:49:33 +0700

    P13

---
+++ P13/webvue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img src="../assets/STIS.png" >
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+
+  <div class="stis">
+    <h1>Project Web STIS</h1>
+    <p>Selamat datang di project Web STIS</p>
+    <p>yang dibuat menggunakan Vue.js</p>
+  </div>
+
+  <footer>
+    <p>Copyright &copy; 2023 Politeknik Statistika STIS</p>
+    <p>Created by Feza Raffa Arnanda</p>
+  </footer>
+</template>
